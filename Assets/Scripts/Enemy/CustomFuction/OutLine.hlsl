a5d6cc37091fcc2aaca79ae8dac2c03288bc8ac6
tris3699 <65750113+tris3699@users.noreply.github.com>
2025-08-19 12:07:53 +0900

    스프라이트 파일 수정

---
+++ Assets/Scripts/Enemy/CustomFuction/OutLine.hlsl
@@ -0,0 +1,29 @@
+﻿void OutLine_float(UnityTexture2D MainTex, float2 UV, float4 Color, float Weight, float2 Size, out float4 RGBA)
+{
+    float4 baseColor = MainTex.Sample(MainTex.samplerstate, UV);    
+    
+    float count = 0.0f;
+
+    for(int y = -1 ; y <= 1; y++)
+    {
+        for(int x = -1; x <= 1; x++)
+        {
+            float2 offset = (float2(x, y) / Size) * Weight;            
+            count += MainTex.Sample(MainTex.samplerstate, UV + offset).a;
+        }
+    }
+
+    float outlineFactor = (count > 0.0f && count < 9.0f) ? 1.0f : 0.0f;
+
+    RGBA = float4(Color.rgb, Color.a * outlineFactor);
+
+    //[branch]
+    //if(count > 0.0f && count < 9)
+    //{
+    //    RGBA = Color;
+    //}
+    //else
+    //{
+    //    RGBA = float4(0, 0, 0, 0);
+    //}
+}
---
+++ Assets/Scripts/Enemy/CustomFuction/OutLineTest.hlsl
@@ -0,0 +1,25 @@
+﻿void OutLineTest_float(float2 UV,UnityTexture2D MainTex,out float4 result)
+{
+    float2 imageSize = float2(1920,1080);
+    float4 outlineColor = float4(1, 0, 0, 1); 
+    float outlineSize = 10.1;
+    float alpha = MainTex.Sample(MainTex.samplerstate, UV).a;
+    float isOutline = 0.0;
+
+    for (int x = -1; x <= 1; x++)
+    {
+        for (int y = -1; y <= 1; y++)
+        {
+            if(x == 0 && y == 0) continue;
+            float2 offset = float2(x, y) * (outlineSize / imageSize);
+            float a = MainTex.Sample(MainTex.samplerstate, clamp(UV + offset, 0.0, 1.0)).a;
+            if (alpha > 0.1 && a < 0.1)
+            {
+                isOutline = 1.0;
+            }
+        }
+    }
+
+    float4 baseColor = MainTex.Sample(MainTex.samplerstate, UV);
+    result = lerp(baseColor, outlineColor, isOutline);
+}
